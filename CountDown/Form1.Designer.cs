4e0a472ac5994c6aef5805ef9849280becb617a6
shay1geo <37382508+shay1geo@users.noreply.github.com>
2020-02-19 14:10:27 -0500

    Add project files.

---
+++ CountDown/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace CountDown
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 15000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Mongolian Baiti", 44.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 63);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "...";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(222, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "16";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(232, 16);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(19, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "15";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(232, 57);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(19, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "17";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(276, 16);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(19, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "45";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(275, 35);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(29, 20);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "00";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(276, 57);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(19, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "15";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(21, 73);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(21, 13);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "set";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(315, 95);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Quittin\' Time";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ CountDown/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CountDown
+{
+    public partial class Form1 : Form
+    {
+        int h = 16;
+        int m = 0;
+        DateTime qt = DateTime.Today;
+        TimeSpan ts = new TimeSpan(16, 0, 0);
+        bool cd; //countdown switch
+        bool set; // set quittin' time
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+            cd = false;
+            set = false;
+            timer1.Start();
+            timer1.Interval = 1;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (timer1.Interval < 10000) { timer1.Interval += 10; } //label8.Text = timer1.Interval + "";}                    
+            if (set) {
+                label1.Hide();
+                label2.Show();
+                label3.Show();
+                label4.Show();
+                label5.Show();
+                label6.Show();
+                label7.Show();
+                label8.Show();
+            }
+            else {
+                label1.Show();
+                label2.Hide();
+                label3.Hide();
+                label4.Hide();
+                label5.Hide();
+                label6.Hide();
+                label7.Hide();
+                label8.Hide();
+            }
+            if (cd)
+            {
+                ts = new TimeSpan(h, m, 0);
+                qt = qt.Date + ts;
+                TimeSpan span = (qt - DateTime.Now);
+                if (timer1.Interval <= 1000) { label1.Text = String.Format("{0}h:{1}m:{2}s", span.Hours, span.Minutes, span.Seconds); }
+                else { label1.Text = String.Format("{0}h:{1}m", span.Hours, span.Minutes); }
+                // no seconds ---label1.Text = String.Format("{0}h:{1}m", span.Hours, span.Minutes);
+                this.Text = "Quittin' Time";
+                label8.Show();
+            }
+            else
+            {
+                label1.Text = DateTime.Now.ToString("hh:mm tt");
+                // seconds ---    label1.Text = DateTime.Now.ToString("hh:mm:ss tt");
+                // 24hr clock --- label1.Text = DateTime.Now.ToString("HH:mm:ss tt");
+                this.Text = "Time";
+            }
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+            cd = !cd; //count down switch toggle
+            timer1.Interval = 100;
+        }
+
+        private void label2_Click(object sender, EventArgs e) { }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+            set = !set;
+            timer1.Interval = 100;
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            h--;
+            if(h < 0) { h = 23; }
+            label3.Text = "" + (h - 1);
+            label4.Text = "" + (h + 1);
+            if (h == 0) { label3.Text = "23"; }            
+            label2.Text = "" + h;
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            {
+                h++;
+                if(h > 23) { h = 0; }
+                label3.Text = "" + (h - 1);
+                label4.Text = "" + (h + 1);
+                if (h == 0) { label3.Text = "23"; }
+                label2.Text = "" + h;
+            }
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+        
+            m = (m-15);
+            if (m < 0) { m = 45; }
+            label5.Text = "" + (m - 15); 
+            label7.Text = "" + (m + 15);
+            label6.Text = "" + m;
+            if (m == 0) { label5.Text = "45"; }
+            if ((m - 15) == 0) { label5.Text = "00"; }
+            if (m == 0) { label6.Text = "00"; }
+            if ((m + 15) >= 60) { label7.Text = "00"; }
+        
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+        
+            m = (m + 15);
+            if (m >= 60) { m = 0; }
+            label5.Text = "" + (m - 15);
+            label7.Text = "" + (m + 15);
+            label6.Text = "" + m;
+            if (m == 0) { label5.Text = "45"; }
+            if ((m - 15) == 0) { label5.Text = "00"; }
+            if (m == 0) { label6.Text = "00"; }
+            if ((m + 15) >= 60) { label7.Text = "00"; }
+        }
+
+
+    }
+
+
+    }
